74a05acccce884ff3636b329f23831e6b8f2c131
Belnor <THINYADANA.24@ichat.sp.edu.sg>
2025-05-28 03:01:16 +0800

    modifying bmi.py

--- bmi.py
+++ bmi.py
@@ -1,3 +1,22 @@
+temp_list = [37, 57, 97]
+def main():
+    print("ET0735(DevOps for AIoT) - Lab2 - Introduction to Python")
+    display_main_menu()
+    temp_list = get_user_input()
+    print(calc_average(temp_list))
+    min_temp, max_temp = find_min_max(temp_list)
+    print("Min Temperature:", min_temp)
+
+def display_main_menu():
+    print("Enter some numbers separated by commmas (e.g, 5, 67, 32)")
+    
+
+
+    
+def calc_average():
+    print("Calculate Average")
+
+
 def calculate_bmi(height,weight):
     print("Height=",height)
     print("Weight=",weight)
@@ -14,8 +33,30 @@ def calculate_bmi(height,weight):
     elif bmi>29.9:
         print("Over weight")
 
+
+def get_user_input():
+    inputstr = input()
+    string_list =inputstr.split(",")
+    float_list = [float(item.strip()) for item in string_list]
+    return float_list
+
+def calc_average(temp_list):
+    return sum(temp_list)/len(temp_list)
+    
+def find_min_max(temp_list):
+    return min(temp_list), max(temp_list)
+
+    
+def sort_temperature():
+    print("sort_temperature")
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+
 calculate_bmi(weight=37, height=1.73)
 
 calculate_bmi(weight=57, height=1.73)
 
 calculate_bmi(weight=97, height=1.73)
+if __name__=="__main__":
+    main()
